f8434e83e14e5a84ad7e5776cc4175e34f7d2805
Saptgiri7 <saptgiriyangal@gmail.com>
2024-12-18 14:47:19 +0530

    changing color added

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const git = `Trying Git fro First Time`
+const git = `Trying Git for First Time`
+
+
+setInterval(()=>{
+    document.body.style.backgroundColor = `#${Math.floor(Math.random() * 256),Math.floor(Math.random() * 256),Math.floor(Math.random() * 256) }`
+},1000)
+
+console.log(Math.floor(Math.random() * 10));
